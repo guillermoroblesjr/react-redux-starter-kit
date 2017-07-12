1e01aa6fc40b17e46b600a26efa890f7bd9d6e2d
Guillermo Robles, Jr <will.robles@gmail.com>
2017-07-12 16:33:30 -0500

    refactor(App.jsx) moved to blueprint files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,13 @@
-import React, { Component, PropTypes } from 'react'
-import _isEqual from 'lodash.isequal'
-import './App.scss'
+import React, { Component } from 'react'
+import { browserHistory, Router } from 'react-router'
+import { Provider } from 'react-redux'
+import PropTypes from 'prop-types'
 
 export default class App extends Component {
-  // static propTypes = {
-  //   reduxState: PropTypes.shape({
-  //     router: PropTypes.object.isRequired,
-  //   }),
-  //   reduxActions: PropTypes.shape({
-  //     actionName: PropTypes.func.isRequired,
-  //   }),
-  // }
-  // static defaultProps = {}
+  static propTypes = {
+    store: PropTypes.object.isRequired,
+    routes: PropTypes.object.isRequired,
+  }
 
   constructor (props) {
     super(props)
@@ -19,15 +15,15 @@ export default class App extends Component {
     this.state = {}
   }
 
-  shouldComponentUpdate = (nextProps, nextState) =>
-    !_isEqual(this.props, nextProps) || !_isEqual(this.state, nextState)
-
-  shouldRenderNull = () => false
+  shouldComponentUpdate = () => false
 
   render = () => {
-    if (this.shouldRenderNull()) return null
     return (
-      <div className="App">App</div>
+      <Provider store={this.props.store}>
+        <div style={{ height: '100%' }}>
+          <Router history={browserHistory} children={this.props.routes} />
+        </div>
+      </Provider>
     )
   }
 }
