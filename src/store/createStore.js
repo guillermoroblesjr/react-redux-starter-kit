e7e563a292d128fb441a25099f04bfa9873fff56
Guillermo Robles, Jr <will.robles@gmail.com>
2017-07-13 16:41:09 -0500

    feat(redux): add redux goodies

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,23 +1,38 @@
 import { applyMiddleware, compose, createStore as createReduxStore } from 'redux'
 import thunk from 'redux-thunk'
 import { browserHistory } from 'react-router'
+import { routerMiddleware } from 'react-router-redux'
+import persistState from 'redux-localstorage'
+import promise from 'redux-promise-middleware'
 import makeRootReducer from './reducers'
 import { updateLocation } from './location'
+import ENV from 'utils/builds/environment'
+
+let createLogger
+if (ENV !== `production`) {
+  createLogger = require(`redux-logger`).createLogger
+}
 
 const createStore = (initialState = {}) => {
   // ======================================================
   // Middleware Configuration
   // ======================================================
-  const middleware = [thunk]
+  const middleware = [thunk, routerMiddleware(history), promise()]
+
+  if (ENV !== `production`) {
+    middleware.push(createLogger())
+  }
 
   // ======================================================
   // Store Enhancers
   // ======================================================
-  const enhancers = []
+  const enhancers = [persistState([
+    // `reducer_key_name_goes_here`,
+  ])]
   let composeEnhancers = compose
 
   if (__DEV__) {
-    if (typeof window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ === 'function') {
+    if (typeof window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ === `function`) {
       composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     }
   }
@@ -39,8 +54,8 @@ const createStore = (initialState = {}) => {
   store.unsubscribeHistory = browserHistory.listen(updateLocation(store))
 
   if (module.hot) {
-    module.hot.accept('./reducers', () => {
-      const reducers = require('./reducers').default
+    module.hot.accept(`./reducers`, () => {
+      const reducers = require(`./reducers`).default
       store.replaceReducer(reducers(store.asyncReducers))
     })
   }
